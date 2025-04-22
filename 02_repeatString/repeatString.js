9c280b60c71c839dc6ba8dbf7cfce02a04fc9ac8
Lydia Schierig <lydiasch.ls@googlemail.com>
2025-04-22 14:52:10 +0200

    Array

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,34 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    let result ="heyheyhey";
 
+    for (let i = 0; i < 3; i++) {
+        console.log(i);
+      }
+    return result;
 };
 
 // Do not edit below this line
 module.exports = repeatString;
+
+
+/* 🔨 Schritt-für-Schritt-Anleitung:
+Parameter hinzufügen
+Deine Funktion braucht zwei Dinge:
+
+Den String, den du wiederholen willst
+
+Die Anzahl, wie oft du ihn wiederholen willst
+👉 Also z. B. repeatString(string, num)
+
+Ergebnis-Variable erstellen
+Eine leere Zeichenkette (let result = "";)
+→ Hier wird später alles zusammengesammelt.
+
+Schleife schreiben
+Eine for-Schleife, die num-mal durchläuft.
+Bei jedem Durchlauf:
+
+hängst du string an result an
+
+Ergebnis zurückgeben
+Nach der Schleife return result */
